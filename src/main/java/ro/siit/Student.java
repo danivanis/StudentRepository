9f758b5a534e28d5ef623e4a5f6ffdf5cf19b97c
dan.ivanis@pm.me <G1tHU8C0Nt+1>
2020-10-25 05:04:52 +0200

    Second version

--- src/main/java/ro/siit/Student.java
+++ src/main/java/ro/siit/Student.java
@@ -1,5 +1,8 @@
 package ro.siit;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Student {
 
     private String firstName;
@@ -7,6 +10,8 @@ public class Student {
     private Integer birthYear;
     private String gender;
     private String CNP;
+
+    public static Logger logger = LogManager.getLogger(App.class.getName());
 
     public Student(String firstName, String lastName, Integer birthYear, String gender, String CNP) {
         this.firstName = firstName;
@@ -17,7 +22,15 @@ public class Student {
     }
 
     public void setFirstName(String firstName) {
-        this.firstName = firstName;
+        if (firstName.isEmpty()) {
+            try {
+                throw new Exception("First name cannot be empty");
+            } catch (Exception exception) {
+                logger.error(exception);
+            }
+        } else {
+            this.firstName = firstName;
+        }
     }
 
     public String getFirstName() {
@@ -25,15 +38,31 @@ public class Student {
     }
 
     public void setLastname(String lastname) {
-        this.lastName = lastName;
+        if (lastName.isEmpty()) {
+            try {
+                throw new Exception("Last name cannot be empty");
+            } catch (Exception exception) {
+                logger.error(exception);
+            }
+        } else {
+            this.lastName = lastName;
+        }
     }
 
     public String getLastname() {
         return lastName;
     }
 
-    public void setBirthYear(int birthYear) {
-        this.birthYear = birthYear;
+    public void setBirthYear(Integer birthYear) {
+        if (birthYear.equals("") || birthYear < 1900 || birthYear > 2020) {
+            try {
+                throw new Exception("Birth year needs to be between 1900 and 2020!");
+            } catch (Exception exception) {
+                logger.error(exception);
+            }
+        } else {
+            this.birthYear = birthYear;
+        }
     }
 
     public Integer getBirthYear() {
@@ -41,12 +70,27 @@ public class Student {
     }
 
     public Integer getAge() {
-        int age = 2020-getBirthYear();
+        Integer age = 2020 - getBirthYear();
         return age;
     }
 
     public void setGender(String gender) {
-        this.gender = gender;
+        if (gender.isEmpty()) {
+            try {
+                throw new Exception("Gender cannot be empty");
+            } catch (Exception exception) {
+                logger.error(exception);
+            }
+        } else if (!(gender.equals("m") || gender.equals("f"))) {
+            try {
+                throw new Exception("Gender must be 'M' or 'F'");
+            } catch (Exception exception) {
+                logger.error(exception);
+            }
+        } else {
+            this.gender = gender;
+        }
+
     }
 
     public String getGender() {
@@ -54,7 +98,21 @@ public class Student {
     }
 
     public void setCNP(String CNP) {
-        this.CNP = CNP;
+        if (CNP.length() < 13) {
+            try {
+                throw new Exception("ID (CNP) must be 13 digits long");
+            } catch (Exception exception) {
+                logger.error(exception);
+            }
+        } else if (!CNP.matches("\\b[1-8]\\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\\d|3[01])(0[1-9]|[1-4]\\d|5[0-2]|99)\\d{4}\\b")) {
+            try {
+                throw new Exception("ID (CNP) must be 13 digits long");
+            } catch (Exception exception) {
+                logger.error(exception);
+            }
+        } else {
+            this.CNP = CNP;
+        }
     }
 
     public String getCNP() {
@@ -70,7 +128,7 @@ public class Student {
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return lastName + " " + getAge() + " years";
     }
 
--- src/test/java/ro/siit/AppTest.java
+++ src/test/java/ro/siit/AppTest.java
@@ -14,21 +14,54 @@ public class AppTest extends TestCase {
 
     private List<Student> students = new ArrayList<>();
 
+    Student student1 = new Student("asa", "", 1, "", "");
+
     public static void main(String[] args) {
 
         AppTest appTest = new AppTest();
 
     }
 
-    public void testAddStudents() {
+    public void testAddStudents(Student student1) {
+        student1.setFirstName("Danut");
+        student1.setLastname("Ivanis");
+        student1.setBirthYear(1987);
+        student1.setGender("m");
+        student1.setCNP("1870101102030");
+        student1.getFirstName();
+        student1.getLastname();
+        student1.getBirthYear();
+        student1.getGender();
+        student1.getCNP();
     }
 
-    public void testRemoveStudents() {
+    public void testRemoveStudents(String CNP) {
+        if (!CNP.isEmpty()) {
+            Student studentToBeDeleted = null;
+            for (Student student : students) {
+                if (student.getCNP().equals(CNP)) {
+                    studentToBeDeleted = student;
+                    break;
+                }
+            }
+            if (studentToBeDeleted != null) {
+                students.remove(studentToBeDeleted);
+            }
+        } else {
+            try {
+                throw new Exception("ID is empty, student does not exist");
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        }
     }
 
     public void testListStudents() {
+        students.sort(new StudentAgeComparatorAscending());
+        for (Student student : students) {
+            System.out.println(student.toString());
+        }
     }
 
-    public void testDisplayMenu() {
-    }
+
 }
